c292cbfc4e6080ef9c9c1cd67f60382185fb95e6
Michael <ruizmic0100@hotmail.com>
2024-05-06 00:00:10 -0500

    Implemented websocket_client.py even more.

--- websocket_client.py
+++ websocket_client.py
@@ -26,5 +26,34 @@ class PSWebsocketClient:
     @classmethod
     async def create(cls, username, password, address):
         self = PSWebsocketClient()
-        self.username = username
-        self.password = 
+        self.username  = username
+        self.password  = password
+        self.address   = "ws://{}/showdown/websocket".format(address)
+        self.websocket = await websockets.connect(self.address)
+        self.login_uri = "https://play.pokemonshowdown.com/action.php"
+        return self
+    
+    async def join_room(self, room_name):
+        message = "/join {}".format(room_name)
+        await self.send_message('', [message])
+        logger.debug("Joined room '{}'".format(room_name))
+
+    async def receive_message(self):
+        message = await self.websocket.recv()
+        logger.debug("Received message from websocket: {}".format(message))
+        return message
+    
+    async def send_message(self, room, message_list):
+        message = room + "|" + "|".join(message_list)
+        logger.debug("Sending message to websocket: {}".format(message))
+        await self.websocket.send(message)
+        self.last_message = message
+
+    async def get_id_and_challstr(self):
+        while True:
+            message = await self.receive_message()
+            split_message = message.split('|')
+            if split_message[1] == 'challstr':
+                return split_message[2], split_message[3]
+            
+    async def login(self):
